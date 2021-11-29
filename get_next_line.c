9fa92cc4c96f18ffa04c5f05ab9bc37c9d6c126b
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2021-11-29 15:47:34 +0100

    3 lignes

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 13:06:35 by nchennaf          #+#    #+#             */
+/*   Updated: 2021/11/29 15:47:26 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static int	i;
+	static char	*buffer;
+
+	buffer = (char*)malloc((sizeof(char) * BUFFER_SIZE) + 1);
+	i = 0;
+	while (read(fd, buffer, BUFFER_SIZE))
+	{
+		while (buffer[i])
+		{
+
+		}
+	}
+}
